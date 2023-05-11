f8355760a1468c92d83e83b884b30f038926613e
nicolas zanardo <nicolas_zanardo@live.fr>
2023-05-11 17:51:14 +0200

    feature_api add function deleteTeamController and middleware on the route

--- server/routes/category/category.js
+++ server/routes/category/category.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const {isLoggedAdmin} = require("../../midelware/isLoggedAdmin");
-const {getAllCategoriesController, updateCategoryController} = require("../../controller/category.controller");
+const {getAllCategoriesController, updateCategoryController, deleteCategoryController} = require("../../controller/category.controller");
 
 
 const category = express.Router();
 
 category.get("/all", isLoggedAdmin, getAllCategoriesController);
 category.put('/update', isLoggedAdmin, updateCategoryController);
-category.delete('/delete/:id')
+category.delete('/delete/:id', isLoggedAdmin, deleteCategoryController)
 
 module.exports = category;
